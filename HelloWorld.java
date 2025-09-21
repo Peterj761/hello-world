d50b55ec86a8bc1f1fb6d0fa3908d5d88ce9fe28
Peter Jordan <peterjordan42605@gmail.com>
2025-09-21 18:41:51 -0400

    Revert "update - Helloworld.java"
    This reverts commit f694872538019047f754203852c14fb44bbe9b50.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,7 @@ public class HelloWorld
 {
    public static void main(String[] args)
    {
-      System.out.println("Hello World");
-      System.out.println("Program modified, but able to revert to original version GitHub Desktop");
-   
-
+      System.out.print("Hello World");
    }
 
 
